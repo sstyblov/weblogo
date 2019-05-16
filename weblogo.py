5f5d0f94cf8e93b3763f19afb79cd9d54ba9124b
sstyblov <sabrina.styblova@gmail.com>
2019-05-16 15:40:39 +0200

    iterate position in msa

--- weblogo.py
+++ weblogo.py
@@ -1,3 +1,14 @@
+msa = list()
+
 file = open('example.msa', 'r')
 for line in file:
-    print(line)
+    line = line.replace('\n', '')
+    if line != '':
+       msa.append(line)
+print(msa)
+
+length = len(msa[0])
+
+for position in range(length):
+    print(position)
+
